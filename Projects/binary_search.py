c417c8ff7f040d9484f8e31cf6d4c6f6b75f57a3
Shakil <tayefehshakila@gmail.com>
2021-12-02 16:04:32 +0100

    Add files via upload

---
+++ Projects/binary_search.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 07:25:35 2017
+
+@author: shakil
+"""
+
+l=[]
+n=int(input("Enter the number of elements:"))
+print("Enter the elements")
+for i in range(0,n):
+    element=float(input())
+    l.append(element)
+x=float(input("What's the number you are looking for?"))
+high=n-1
+low=0
+found=False
+while(low<=high and found==False):
+    m=int((high+low)/2)
+    if(x==l[m]):
+        print("Found")
+        found=True
+        break
+    elif(x<l[m]):
+        high=m-1
+    else:
+        low=m+1
+if(found==False):
+    print("Not found")
+    
---
+++ Projects/linear_search.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 07:19:46 2017
+
+@author: shakil
+"""
+
+l=[2,98,45,89,658,7.59,78,5,5300]
+x=78
+for i in range(0,len(l)):
+    if(l[i]==x):
+        print("Found")
+
